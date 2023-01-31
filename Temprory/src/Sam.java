c4d29004d57fb85245d0d5029b6c9b873a1600d1
Samyak Khabiya <samyakkhabiya123@gmail.com>
2023-01-31 15:37:11 +0530

    update 2

--- Temprory/src/Sam.java
+++ Temprory/src/Sam.java
@@ -1,9 +1,13 @@
+import pack1.MyInterfaceImp;
 
 public class Sam {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello");
+//		System.out.println("hello");
+		MyInterfaceImp impl=new MyInterfaceImp();
+		impl.disp();
+ 
 	}
 
 }
